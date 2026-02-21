d023b4887fb3204451826f83d21ae9543e596d92
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-21 14:51:24 -0500

    Updated initial project files.

---
+++ portfolio/source/App.jsx
@@ -0,0 +1,19 @@
+// IMORTED CUSTOM COMPONENTS
+import Nav from "./components/Nav/Nav";
+import Hero from "./components/Hero/Hero";
+// IMORTED STYLESHEETS
+import "./assets/css/variables.css";
+import "./assets/css/global.css";
+
+const App = function () {
+	return (
+		<>
+			<Nav />
+			<div className="div-main-edge-container">
+				<Hero />
+			</div>
+		</>
+	);
+};
+
+export default App;
--- portfolio/source/main.jsx
+++ portfolio/source/main.jsx
@@ -1,5 +1,7 @@
+// IMORTED CORE COMPONENTS
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+// IMORTED CUSTOM COMPONENTS
 import App from "./App";
 
 const root = createRoot(document.getElementById("root"));
