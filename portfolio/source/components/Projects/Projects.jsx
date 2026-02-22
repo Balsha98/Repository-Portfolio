17ec7f2ba785fc684d3b16746ec576b788119fe9
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-21 22:40:20 -0500

    Added Projects component.

---
+++ portfolio/source/components/Projects/Projects.jsx
@@ -0,0 +1,110 @@
+// IMORTED STYLESHEETS
+import "./css/Projects.css";
+import "./css/Media.css";
+
+const Projects = function () {
+	return (
+		<section id="#work" className="section-projects-container">
+			<header className="header-section-projects-container">
+				<span>01 / Work</span>
+				<h2>Selected Projects</h2>
+			</header>
+			<div className="div-project-cards-container">
+				<div className="div-project-card-container">
+					<div className="div-project-card-overview-container">
+						<header className="header-project-card-overview-container">
+							<span>Streaming &mdash; SaaS Platform</span>
+							<h2>Some App Name</h2>
+						</header>
+						<div className="div-project-card-info-container">
+							<p>
+								Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
+								possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
+							</p>
+							<ul className="project-card-tech-stack-list">
+								<li className="project-card-tech-stack-list-item">
+									<span>JavaScript</span>
+								</li>
+								<li className="project-card-tech-stack-list-item">
+									<span>React.js</span>
+								</li>
+								<li className="project-card-tech-stack-list-item">
+									<span>Node.js</span>
+								</li>
+								<li className="project-card-tech-stack-list-item">
+									<span>CSS3</span>
+								</li>
+								<li className="project-card-tech-stack-list-item">
+									<span>Supabase</span>
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="div-project-card-thumbnail-container">
+						<span>Pinnacle</span>
+					</div>
+				</div>
+				<div className="div-project-card-container">
+					<header className="header-project-card-overview-container">
+						<span>Development &mdash; SaaS Platform</span>
+						<h2>Some App Name</h2>
+					</header>
+					<div className="div-project-card-info-container">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
+							possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
+						</p>
+						<ul className="project-card-tech-stack-list">
+							<li className="project-card-tech-stack-list-item">
+								<span>JavaScript</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>React.js</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>Node.js</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>CSS3</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>Supabase</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+				<div className="div-project-card-container">
+					<header className="header-project-card-overview-container">
+						<span>Sports &mdash; SaaS Platform</span>
+						<h2>Some App Name</h2>
+					</header>
+					<div className="div-project-card-info-container">
+						<p>
+							Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
+							possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
+						</p>
+						<ul className="project-card-tech-stack-list">
+							<li className="project-card-tech-stack-list-item">
+								<span>JavaScript</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>React.js</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>Node.js</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>CSS3</span>
+							</li>
+							<li className="project-card-tech-stack-list-item">
+								<span>Supabase</span>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default Projects;
