a8682e7f834e3223868811b5e19fe3b288a9c46a
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-21 22:55:42 -0500

    Updated Projects component.

--- portfolio/source/components/Projects/Projects.jsx
+++ portfolio/source/components/Projects/Projects.jsx
@@ -9,94 +9,94 @@ const Projects = function () {
 				<span>01 / Work</span>
 				<h2>Selected Projects</h2>
 			</header>
-			<div className="div-project-cards-container">
-				<div className="div-project-card-container">
-					<div className="div-project-card-overview-container">
-						<header className="header-project-card-overview-container">
+			<div className="div-projects-cards-container">
+				<div className="div-projects-card-container">
+					<div className="div-projects-card-overview-container">
+						<header className="header-projects-card-overview-container">
 							<span>Streaming &mdash; SaaS Platform</span>
 							<h2>Some App Name</h2>
 						</header>
-						<div className="div-project-card-info-container">
+						<div className="div-projects-card-info-container">
 							<p>
 								Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
 								possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
 							</p>
-							<ul className="project-card-tech-stack-list">
-								<li className="project-card-tech-stack-list-item">
+							<ul className="projects-card-tech-stack-list">
+								<li className="projects-card-tech-stack-list-item">
 									<span>JavaScript</span>
 								</li>
-								<li className="project-card-tech-stack-list-item">
+								<li className="projects-card-tech-stack-list-item">
 									<span>React.js</span>
 								</li>
-								<li className="project-card-tech-stack-list-item">
+								<li className="projects-card-tech-stack-list-item">
 									<span>Node.js</span>
 								</li>
-								<li className="project-card-tech-stack-list-item">
+								<li className="projects-card-tech-stack-list-item">
 									<span>CSS3</span>
 								</li>
-								<li className="project-card-tech-stack-list-item">
+								<li className="projects-card-tech-stack-list-item">
 									<span>Supabase</span>
 								</li>
 							</ul>
 						</div>
 					</div>
-					<div className="div-project-card-thumbnail-container">
+					<div className="div-projects-card-thumbnail-container">
 						<span>Pinnacle</span>
 					</div>
 				</div>
-				<div className="div-project-card-container">
-					<header className="header-project-card-overview-container">
+				<div className="div-projects-card-container">
+					<header className="header-projects-card-overview-container">
 						<span>Development &mdash; SaaS Platform</span>
 						<h2>Some App Name</h2>
 					</header>
-					<div className="div-project-card-info-container">
+					<div className="div-projects-card-info-container">
 						<p>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
 							possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
 						</p>
-						<ul className="project-card-tech-stack-list">
-							<li className="project-card-tech-stack-list-item">
+						<ul className="projects-card-tech-stack-list">
+							<li className="projects-card-tech-stack-list-item">
 								<span>JavaScript</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>React.js</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>Node.js</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>CSS3</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>Supabase</span>
 							</li>
 						</ul>
 					</div>
 				</div>
-				<div className="div-project-card-container">
-					<header className="header-project-card-overview-container">
+				<div className="div-projects-card-container">
+					<header className="header-projects-card-overview-container">
 						<span>Sports &mdash; SaaS Platform</span>
 						<h2>Some App Name</h2>
 					</header>
-					<div className="div-project-card-info-container">
+					<div className="div-projects-card-info-container">
 						<p>
 							Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis in, neque doloribus aperiam, fugit, officia nesciunt repellat
 							possimus beatae quasi reprehenderit repellendus hic inventore culpa ex incidunt consectetur. Hic, distinctio?
 						</p>
-						<ul className="project-card-tech-stack-list">
-							<li className="project-card-tech-stack-list-item">
+						<ul className="projects-card-tech-stack-list">
+							<li className="projects-card-tech-stack-list-item">
 								<span>JavaScript</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>React.js</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>Node.js</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>CSS3</span>
 							</li>
-							<li className="project-card-tech-stack-list-item">
+							<li className="projects-card-tech-stack-list-item">
 								<span>Supabase</span>
 							</li>
 						</ul>
