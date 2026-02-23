25a4a2ff30616517df5623d304ec4add4cc086dc
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-22 19:09:48 -0500

    Updated Hero component.

--- portfolio/source/components/Hero/Hero.jsx
+++ portfolio/source/components/Hero/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = function () {
 			<section className="section-hero-container">
 				<header className="header-section-hero-container">
 					<span>Available for Hire</span>
-					<h2 className="header-section-hero-container-heading">
+					<h1 className="header-section-hero-container-heading">
 						<span>Frontend</span>
 						<span>Engineer</span>
-					</h2>
+					</h1>
 				</header>
 				<div className="div-section-hero-overview-container">
 					<p>I build fast, accessible interfaces that people actually enjoy using. 3+ years turning design into pixel-perfect, performant reality.</p>
