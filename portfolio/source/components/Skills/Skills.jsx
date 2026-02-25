cd7ea652913a82bc6017b3970dc9bebccbc0ad2a
Balša Bazović <balsa.bazovic@gmail.com>
2026-02-25 06:28:38 -0500

    Updated Skills component responsiveness.

--- portfolio/source/components/Skills/Skills.jsx
+++ portfolio/source/components/Skills/Skills.jsx
@@ -3,108 +3,108 @@ import "./css/Skills.css";
 import "./css/Media.css";
 
 const Skills = function () {
-	return (
-		<section id="skills" className="section-skills-container">
-			<header className="header-section-skills-container">
-				<span>02 / Skills</span>
-				<h2>Technical Arsenal</h2>
-			</header>
-			<div className="div-skills-cards-container">
-				<div className="div-skills-card-container">
-					<header className="header-skills-card-container">
-						<h4>Languages</h4>
-					</header>
-					<ul className="skills-card-tech-stack-list">
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-					</ul>
-				</div>
-				<div className="div-skills-card-container">
-					<header className="header-skills-card-container">
-						<h4>Frameworks</h4>
-					</header>
-					<ul className="skills-card-tech-stack-list">
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-					</ul>
-				</div>
-				<div className="div-skills-card-container">
-					<header className="header-skills-card-container">
-						<h4>Tooling</h4>
-					</header>
-					<ul className="skills-card-tech-stack-list">
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-					</ul>
-				</div>
-				<div className="div-skills-card-container">
-					<header className="header-skills-card-container">
-						<h4>Design</h4>
-					</header>
-					<ul className="skills-card-tech-stack-list">
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-						<li className="skills-card-tech-stack-list-item">
-							<p>Tech Name</p>
-							<span />
-						</li>
-					</ul>
-				</div>
-			</div>
-		</section>
-	);
+    return (
+        <section id="skills" className="section-skills-container">
+            <header className="header-section-skills-container">
+                <span>02 / Skills</span>
+                <h2>Technical Arsenal</h2>
+            </header>
+            <div className="div-skills-cards-container">
+                <div className="div-skills-card-container">
+                    <header className="header-skills-card-container">
+                        <h4>Languages</h4>
+                    </header>
+                    <ul className="skills-card-tech-stack-list">
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Java</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Python</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>JavaScript</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>PHP</p>
+                            <span />
+                        </li>
+                    </ul>
+                </div>
+                <div className="div-skills-card-container">
+                    <header className="header-skills-card-container">
+                        <h4>Frameworks</h4>
+                    </header>
+                    <ul className="skills-card-tech-stack-list">
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>React.js / Next.js</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Node.js / Nest.js</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Flutter</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>SwiftUI</p>
+                            <span />
+                        </li>
+                    </ul>
+                </div>
+                <div className="div-skills-card-container">
+                    <header className="header-skills-card-container">
+                        <h4>Tooling</h4>
+                    </header>
+                    <ul className="skills-card-tech-stack-list">
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Vite</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>CI/CD Pipelines</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>GitHub</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Docker</p>
+                            <span />
+                        </li>
+                    </ul>
+                </div>
+                <div className="div-skills-card-container">
+                    <header className="header-skills-card-container">
+                        <h4>Design</h4>
+                    </header>
+                    <ul className="skills-card-tech-stack-list">
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Figma</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Photoshop / Illustrator</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Lucidchart</p>
+                            <span />
+                        </li>
+                        <li className="skills-card-tech-stack-list-item">
+                            <p>Canva</p>
+                            <span />
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default Skills;
