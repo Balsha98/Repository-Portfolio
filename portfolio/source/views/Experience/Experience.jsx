7fe48e312428e005ac3a47434e033106ff28672c
Balša Bazović <balsa.bazovic@gmail.com>
2026-06-24 23:40:44 -0400

    Updated the Experience component.

--- portfolio/source/views/Experience/Experience.jsx
+++ portfolio/source/views/Experience/Experience.jsx
@@ -8,7 +8,7 @@ const EXPERIENCES = [
         position: "Full-Stack Engineer",
         company: "EZ-Booker (Contract)",
         description:
-            "Co-maintained the software solution made in React.js and Python with the company's CEO. Communicated with stakeholders regarding application upgrades and bug fixes. Helped build the software solution that led to its recent acquisition by Hoteza.",
+            "Co-maintained the software solution made in React.js and PHP with the company's CEO. Communicated with stakeholders regarding application upgrades and bug fixes. Helped build the software solution that led to its recent acquisition by Hoteza.",
     },
     {
         timeline: "2024 — 2024",
@@ -34,23 +34,28 @@ const Experience = function () {
                 <h2>Where I've Worked</h2>
             </header>
             <ul className="section-experience-positions-list">
-                {EXPERIENCES.map(({ timeline, position, company, description }, i) => {
-                    return (
-                        <li key={i} className="section-experience-positions-list-item">
-                            <span>{timeline}</span>
-                            <div className="div-section-experience-positions-list-item-content-container">
-                                <header className="header-section-experience-positions-list-item">
-                                    <h4>{position}</h4>
-                                    <div className="div-header-section-experience-positions-list-item-company-container">
-                                        <ion-icon src="/media/icons/arrow-right.svg" />
-                                        <span>{company}</span>
-                                    </div>
-                                </header>
-                                <p>{description}</p>
-                            </div>
-                        </li>
-                    );
-                })}
+                {EXPERIENCES.map(
+                    ({ timeline, position, company, description }, i) => {
+                        return (
+                            <li
+                                key={i}
+                                className="section-experience-positions-list-item"
+                            >
+                                <span>{timeline}</span>
+                                <div className="div-section-experience-positions-list-item-content-container">
+                                    <header className="header-section-experience-positions-list-item">
+                                        <h4>{position}</h4>
+                                        <div className="div-header-section-experience-positions-list-item-company-container">
+                                            <ion-icon src="/media/icons/arrow-right.svg" />
+                                            <span>{company}</span>
+                                        </div>
+                                    </header>
+                                    <p>{description}</p>
+                                </div>
+                            </li>
+                        );
+                    },
+                )}
             </ul>
         </section>
     );
